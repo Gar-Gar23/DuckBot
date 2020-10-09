d6d8a2f47c46ab41cd680bbf98836b9495a092f1
Gar-Gar23 <g.dambra233@gmail.com>
2020-10-09 18:18:28 -0500

    added a utility function for corpuses, and added mesg logs

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const fs = require("fs").promises
 var {create} = require("@kit-js/core/js/util");
 const Discord = require('discord.js');
-const ai = require('@kettlelogic/language-model')
+const ai = require('@kettlelogic/language-model');
+const { readFile } = require('fs');
 
 //duck needs to "say"
 
@@ -25,15 +26,28 @@ var m = create(ai.Model)();
 /// casheing msg, 
 client.on('ready', async() => {
     console.log(`Logged in as ${client.user.tag}!`);
-    const shakespere = await fs.readFile('shakespere.txt','utf8')
-    m.train(2,shakespere.split(/\W+/))
+    async function readCorpusFromFile(filename){
+        const shakespere = await fs.readFile(filename ,'utf8')
+        m.train(2,shakespere.split(/\W+/)) 
+    } 
+    async function readCorpusFromFiles(fileNames){
+        for(let name of fileNames) {
+            await readCorpusFromFile(name);
+        }
+    }
+    await readCorpusFromFiles(['shakespere.txt', 'hacker-howto.txt'])
+    console.log('done reading corpus files')
+    
+
 });
 client.on('message', async msg => {
+    console.log(msg.author.tag, ':', msg.channel.name, ':', msg.channel.guild.name, ':',msg.content)
     m.train(2,msg.content.split(/\W+/))
     if(msg.author.tag !== "Duck#4445")
     {
-        msg.reply(m.generateRandomPhrase(10))
+        msg.reply(m.generateRandomPhrase(20))
     }
     
 });
 client.login(process.env.DISCORD_API_KEY);
+/// add function async , remove calls read Corpus From file, add singular call to the  function corpus
