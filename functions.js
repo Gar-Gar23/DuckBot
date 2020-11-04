6d40288f76e967452da33fc09ad3879b762a7a65
Gar-Gar23 <g.dambra233@gmail.com>
2020-11-03 18:47:41 -0600

    commsLink

---
+++ functions.js
@@ -0,0 +1,11 @@
+/*              Functions
+
+            a block of code to perform a particular task
+            functions are executed when they are (called) or (invoked)
+       functions are wirtten function, name, () 
+    function names follow the same rules as declaring variables    
+    code to be executed is written in {} 
+
+    function Tubers(spuds, turnips, ginger) *** what is being executed***
+     {peeled and boiled}
+
