7ebb86a7e85d610729f1620ae9af474077aa97b7
Gar-Gar23 <g.dambra233@gmail.com>
2020-11-03 18:59:37 -0600

    commsLink

---
+++ tutorials/syntax.js
@@ -0,0 +1,25 @@
+console.log('Hello, Dolly')
+
+var a, b, c;
+a = 5
+b = 6
+c = a + b;
+
+var n, m, l;
+n = 8; m = 9; l = n + m;
+
+/*          key words
+
+break     terminates a switch or loop
+continue  jumps out of loop and starts back at top
+
+debugger stops and tries to pull the de bug function
+
+i... else makes block of code to be wxecuted 
+
+return exits a function
+ try ... catch implements error handeling block
+
+ var declares whatever as a variable
+ */ 
+
