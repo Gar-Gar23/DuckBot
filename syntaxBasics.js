a2fc17dc6b854022a8113759f77870db70b72581
Gar-Gar23 <g.dambra233@gmail.com>
2020-11-04 13:02:16 -0600

    here is my noted log on Syntax

---
+++ syntaxBasics.js
@@ -0,0 +1,57 @@
+//               syntaax basics     Syntax is the set of rules, and how JS programs are written
+
+var x, y, z /* first we must declare what variables er are working with
+            variables can be strings, numbers, or anything that is definde as an object
+*/ 
+x =7; y = 8;  // here we are assigning the varible a value 
+z = x + y      // here we are computing those values
+
+/* we must make sure that each variable is clearly assigned and computed otherwise sytax errors
+the variables can be assigned either sharing the line using the ';' or we can seperate with 
+individual lines
+x = 7
+y = 8
+z = x + y
+
+        JS syntax with two types  
+    fixed values which are called literals
+    literals are numbers with or without decimals and 'strings' 'single' or "double "
+    quotes are accepted in JS
+
+    and variable values called.... variables    *creative*
+    variable are used to store DATA VALUES  
+    the var keyword is used to DECLARE variables
+     the '=' is used as an assignment operator  * it is NOT "equal to" like in normal math*
+*/
+
+var r; // here wer have defined a variable as x
+ r = 2 // here we have assigned the value of 2 to r
+
+ /* operators ( + - * / ) are used to compute values
+            addition, subtraction, multiplication, and division
+    = is used to assign values to variables
+
+    an expression is the combo of variables, values, and operators
+    which make a computation. which is then called an expression
+
+            Expressions can contain variable values  x / 2 
+            values can strings or numbers  such as 'shield' + ' ' + 'wall'
+
+
+    JS identifiers 
+
+    Identifiers are used to name variables, keywords, functions and labels
+    identifiers must start with a letter, _underscore, or a $
+    anything after can be any characters
+
+        JS is case sensitive 
+*/
+var lastname, lastName;// are different variables
+lastname = beavers
+lastName = Dambra
+
+/*    JS most compound words are written in camelCase 
+hyphens are reserved for the subtration operator
+
+althogh under_scores 
+and PascalCase are also used  JS programmers tend to opt for camelCase
