9fcc0b52ca7612ece6079ef010680ccb04755a40
Gar-Gar23 <g.dambra233@gmail.com>
2020-11-15 20:52:56 -0600

    operators and what they do

---
+++ operator.js
@@ -0,0 +1,31 @@
+            /*JS operators
+    remember nothing is ' equal to until == is written otherwise we are just assigning values
+*/
+    var x = 5           // assinged value to x
+    var y = 2           // assigned value to y
+    var z = x + y       // assigned value to z
+
+
+                //  '=' is the assignment operator
+                var plop = plop
+            
+                var l = 5       // assigned value
+                var m = 8       // assigned value
+                var n = l + m   // assineged  addidion operators and arithmatic
+                //  n = l += m   alternate addition operatore '+=' is addititob
+
+                var a = 4       // assinged value to variable a
+                var b = 3       // assinged value to variable b
+                var c = a * b   // assigned multiplication operator and a value to variable c
+                // var c = a *= n  alternate operator of '*=' is multiplication
+                var d = 7       // assinged value to variable d    
+                var e = 4       // assigned value to variable 
+                var f =  d - e  // aassigned subtraction operator and value to variable
+                // var f = d -= e  alternate operatorof -= is subtratction
+
+                var t = 9        // assigned value to variable
+                var u = 3        // assinged value to the variable
+                var v = t / u    // assigninged division operatior to find value of t
+                // var v = t /= u  altternate operator of /= is division
+
+                
