ba6430d19404f5538d3f52e5c5de397f0acb7118
Gar-Gar23 <g.dambra233@gmail.com>
2020-11-15 20:45:10 -0600

    basics on what text strings are

---
+++ text.string.js
@@ -0,0 +1,36 @@
+            //    JS string operations
+            var x = 'Garrett';     // assigned a text string value to variable x
+            var y = "Arden"        // assinged text string to variable y
+            var z = x + ' ' + y    // the + operator  adds strings together ( concatenation )         
+            console.log(z)         //  the " "  is whites space as to ad a "space" between txt  
+    
+        // concatenation  can add phrases
+        
+    var text1 = 'you are what,';
+    text1 += ' you eat!'
+    console.log(text1)
+
+            /*  computing two numbers,, will return a sum, but addiing a number and a string 
+                will return a string */
+             
+    var o = 20 - 10; // computing 2 numbers returns a number
+    var m = '10' + 5; // a number that is a string added to a number returns string + number
+        var n = 'dogg' + 00; // string added to a string retuns txt strng added to number
+ console.log(o,m,n)
+
+
+ /* comparisons operators
+
+  == equal to
+  === equal value and equal type
+  != not equal
+  !== not equal to value and not equal to type 
+  > greather
+  < less than
+  >= equal or greater to
+  <= less than or equalto
+  ? temerary holder 
+   Logic operators 
+    && and
+    || or
+    ! not 
